1e85840d85f5b886603284fb2c257c7007f529ea
vitoremanueldev <veslima3@gmail.com>
2020-09-17 01:09:00 -0300

    Hexadecimal to decimal calculator

--- hexadecimal_to_decimal/src/hexadecimal_to_decimal.c
+++ hexadecimal_to_decimal/src/hexadecimal_to_decimal.c
@@ -5,6 +5,8 @@
  Version     :
  Copyright   : 
  Description : hexadecimal to decimal conversion ex: 7DE = (7 * 16²) + (13 * 16¹) + (14 * 16⁰)
+ solution to an IDE problem using pow():
+ https://www.eclipse.org/forums/index.php?t=msg&th=68204/
  ============================================================================
  */
 
@@ -16,38 +18,59 @@
 int main(){
 
 	char hexadecimal[SIZE];
-	char extractValue[SIZE] = {'0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'E', 'F'};
+	char extractValue[SIZE] = {'0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 	printf("Type a hexadecimal number:");
 	fgets(hexadecimal, SIZE, stdin);
 
-		//make sure the characters are all uppercase
+	//make sure the characters are all uppercase
 
-		int current = 0;
+	int current = 0;
 
-		while(hexadecimal[current] != '\0'){
+	while(hexadecimal[current] != '\0'){
 
-			if(hexadecimal[current] >= 'a' && hexadecimal[current] <= 'z'){
+		if(hexadecimal[current] >= 'a' && hexadecimal[current] <= 'z'){
 
-				hexadecimal[current] -= 32;
-
-			}
-
-			current++;
+			hexadecimal[current] -= 32;
 
 		}
 
-		// find the exponent's value
-		int exponent = 0;//gathering this value to use it on the pow function
+		current++;
 
-		for(int current = 0; hexadecimal[current] != 0; current++, exponent++){
+	}
 
-			printf("%c", hexadecimal[current]);
+	// find the exponent's value
+	int exponent = 0;//gathering this value to use it on the pow function
 
-		}
+	for(int current = 0; hexadecimal[current] != 0; current++, exponent++){
 
-		exponent -= 1;
+		printf("%c", hexadecimal[current]);
+
+	}
+
+	exponent -= 2;//take out the excess. if we don't we'll get a 0 and some garbage
+
+	//loop to attribute an integer value to the char elements
+	int decimal = 0;
+
+    for(int current = 0; hexadecimal[current] != 0; current++){
+
+        //printf("%c", hexadecimal[current]);
+        for(int value = 0; value < 16; value++){
+
+            //printf("%c", extractValue[value]);
+            if(extractValue[value] == hexadecimal[current]){
+
+	            decimal += value * pow(16, exponent);
+	            exponent--;
+
+	        }
+
+         }
 
 
+    }
+
+	printf("%s corresponds to %d in decimal.", hexadecimal, decimal);
 
 }
