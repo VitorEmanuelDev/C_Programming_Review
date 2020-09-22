5250e26576bfdc8f35050eebc454e16afe96db88
vitoremanueldev <veslima3@gmail.com>
2020-09-22 02:18:12 -0300

    Sort an int array

--- sort_array/src/sort_array.c
+++ sort_array/src/sort_array.c
@@ -13,6 +13,30 @@
 
 int main(void) {
 
+	int size = 10;
+	int array[size] = {9,7,5,6,4,2,3,1,8,0};
+	int temp = 0;
 
+	for(int currentX = 0; currentX < size; currentX++){
+
+		for(int currentY = 0; currentY < size; currentY++){
+
+			if(array[currentX] < array[currentY]){
+
+				temp = array[currentX];
+				array[currentX] = array[currentY];
+				array[currentY] = temp;
+
+			}
+
+		}
+
+	}
+
+	for(int current = 0; current < size; current++){
+
+		printf("%d ", array[current]);
+
+	}
 
 }
